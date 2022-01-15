5ee53f708e0088abbf45703dc8d83c8802e55adb
pushpa3096 <pushpa3096@gmail.com>
2022-01-15 11:47:54 +0530

    Adding user login ID

--- src/main/java/com/vinit/feedback/controller/GreetingController.java
+++ src/main/java/com/vinit/feedback/controller/GreetingController.java
@@ -1,24 +1,35 @@
 package com.vinit.feedback.controller;
 
 import com.vinit.feedback.bean.Greeting;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.java.Log;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v1")
+@Log
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "User") String name, @RequestHeader Map<String, String> headers,
+                             Authentication authentication, Principal principal) {
+        headers.forEach((key, value) -> {
+            log.info(String.format("Header '%s' = %s", key, value));
+        });
+        System.out.println(authentication.getName());
+        System.out.println("-----------------");
+        System.out.println(principal.getName());
+        name =(null!=authentication.getName())?authentication.getName():"User";
+
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }
