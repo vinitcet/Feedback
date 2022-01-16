695c449e31aac8a01d0c2deeb22f4feb5e8d8f05
pushpa3096 <pushpa3096@gmail.com>
2022-01-16 23:11:34 +0530

    Encrypting password

--- src/main/java/com/vinit/feedback/FeedbackApplication.java
+++ src/main/java/com/vinit/feedback/FeedbackApplication.java
@@ -1,5 +1,6 @@
 package com.vinit.feedback;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -8,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication
 @EnableCaching
 @EnableAsync
+@EnableEncryptableProperties
 //@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
 public class FeedbackApplication {
 
